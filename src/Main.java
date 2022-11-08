5a5de99ecf083231aeaa8501b7a8fe390ccb34c3
Wallison Ferreira <sanfer_wallison@hotmail.com>
2022-11-08 14:57:15 -0500

    Atividade 1

--- src/Main.java
+++ src/Main.java
@@ -4,22 +4,27 @@ public class Main {
 
 	public static void main(String[] args) {
 		
-		int y = 32;
-		double x = 10.35784;
-		String nome = "Wallison";
-		int idade = 28;
-		double renda = 5000.00;
+		String product1 = "Computer";
+		String product2 = "Office desk";
 		
-		System.out.println(y);
-		System.out.println(x);
-		System.out.printf("%.2f%n", x);
-		System.out.printf("%.4f%n", x);
+		int age = 30;
+		int code = 5290;
+		char gender = 'F';
+		
+		double price1 = 2100.0;
+		double price2 = 650.50;
+		double measure = 53.234567;
+		
+		System.out.println("Products:");
+		System.out.printf("%s, which price is $ %.2f%n", product1, price1);
+		System.out.printf("%s, which price is $ %.2f%n", product2, price2);
+		System.out.println("");
+		System.out.printf("Record: %d years old, code %d and gender: %s", age, code, gender);
+		System.out.println("");
+		System.out.printf("Measure with eight decimal places: %.8f%n", measure);
+		System.out.printf("Rounded (three decimal places): %.3f%n", measure);
 		Locale.setDefault(Locale.US);
-		System.out.printf("%.4f%n", x);
-		System.out.println("RESULTADO = " + x + " METROS");
-		System.out.printf("RESULTADO = %.2f METROS%n", x);
-		System.out.println("RESULTADO = " + x + " METROS");
-		System.out.printf("Nome: %s%n" + "Idade: %d%n" + "Renda: %f%n", nome, idade, renda);
+		System.out.printf("US decimal point: %.3f", measure);
 	}
 
 }
